32afaa2bb829152b23ce3d1d81c8e512b7c59687
Mathew Bee <adeniyi.m1007@gmail.com>
2022-02-14 01:25:17 +0100

    Added upload to zoho function

---
+++ uploader.php
@@ -0,0 +1,36 @@
+<?php
+echo uploadItem("Item Mathew From Script 4029","1000","Just a desc","imgLink","partNo","ModelNum",true);
+
+function uploadItem($item_name,$item_price,$item_desc,$item_img,$item_part_no,$item_model_num,$uploadScrapedData=false){
+    
+    if($uploadScrapedData) {
+        $data = [
+            'item_scraped_name' => $item_name,
+            'item_scraped_price' => $item_price,
+            'item_description' => $item_desc,
+            'item_img' => $item_img,
+            'item_part_no' => $item_part_no,
+            'item_model_no' => $item_model_num
+        ];
+        
+        $handle = curl_init('https://flow.zoho.com/771863076/flow/webhook/incoming?zapikey=1001.6d43c16859efed4e76b58670c5e8d9cb.7d41bb9f2305ba2929f041a9f491d7ca&isdebug=false'); 
+        
+        $encodedData = $data;
+        curl_setopt($handle, CURLOPT_POST, 1);
+        curl_setopt($handle, CURLOPT_POSTFIELDS, $encodedData);
+        curl_setopt($handle, CURLOPT_FAILONERROR, true); 
+        curl_setopt($handle, CURLOPT_FOLLOWLOCATION, true); 
+        curl_setopt($handle, CURLOPT_RETURNTRANSFER, true); 
+        curl_setopt($handle, CURLOPT_SSL_VERIFYHOST, false); 
+        curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);   
+        $result = curl_exec($handle);
+        $httpcode = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+        // print_r($result = json_decode($result, true));
+        curl_close($handle);
+        return $httpcode;
+    }
+}
+
+
+
+?>
